10b2c023ae4a193d250151dc6d842a126b0ccb53
honnage <hon_da-1998@hotmail.com>
2021-11-20 23:58:19 +0700

    Middleware

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,7 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard', compact('users'));
 })->name('dashboard');
 
-
-Route::get('/department/all',[DepartmentController::class,'index'])->name('department');
-Route::post('/department/add',[DepartmentController::class,'store'])->name('addDepartment');
+Route::middleware(['auth:sanctum', 'verified'])->group(function(){
+    Route::get('/department/all',[DepartmentController::class,'index'])->name('department');
+    Route::post('/department/add',[DepartmentController::class,'store'])->name('addDepartment');
+});
