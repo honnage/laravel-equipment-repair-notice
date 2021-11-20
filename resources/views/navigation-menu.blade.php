3aac882cec7394d95ce3207a0a21e5329879b4dc
honnage <hon_da-1998@hotmail.com>
2021-11-20 20:10:01 +0700

    Select user and diffForHumans

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -13,7 +13,8 @@
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                        {{-- {{ __('Dashboard') }} --}}
+                        User
                     </x-jet-nav-link>
                 </div>
             </div>
